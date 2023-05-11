374ce6da3ee270f91ec97ed44c50d6f1e450910a
elwinspranata <elwinpranata02@gmail.com>
2023-05-11 10:40:07 +0700

    Add project files.

---
+++ exes_2_022_A/exes_2_022_A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+{
+  Array_search(elwins, x) :
+    lowerbound = 0
+    upperbound = len(elwins) - 1
+
+  while lowerbound <= upperbound :
+       mid = (lowerbound + upperbound)
+}
+ if arr[mid] == x :
+    return mid
+    elif arr[elwins] < x :
+    lowerbound = mid + 1
+      else:
+upperbound = mid - 1
+ return -1
+ arr = [33, 10, 2, 8, 2, 5]
+ x = 25
+result = Array_search(elwins, x)
+{
+    if result != -1:
+    print
+    else:
+    print
+};
+  
